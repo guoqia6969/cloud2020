ba4efcd2b1779b0dd8bfd7c09dd4aa47b014577f
wangsh <247873964@qq.com>
2020-10-21 21:27:25 +0800

    增加restTemplate getForEntity方法

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -16,6 +16,9 @@ public class ApplicationContextConfig {
 
     @Bean
     @LoadBalanced //使用@LoadBalanced注解赋予RestTemplate负载均衡的能力
+    /**
+     * 获取到restTemplate对象
+     */
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,9 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
     //public static final String PAYMENT_URL = "http://localhost:8001";
+    /**
+     * 链接的服务器地址
+     */
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
@@ -34,6 +38,23 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
+
+    /**
+     * restTemplate getForEntity方法
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            log.info(entity.getStatusCode()+"\t"+entity.getBody());
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+            
 
 
 }
